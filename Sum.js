9b1d48f01b72b04f0aaad99f2cc88aca92280a82
sarita B.K. <bksarita729@gmail.com>
2023-09-24 14:52:01 +0545

    JavascriptCode

---
+++ Sum.js
@@ -0,0 +1,9 @@
+/*let a=10;
+let b=20;
+var c=(a+b);
+console.log("the sum of "+a+" and "+b+"is:",c);
+*/
+let a=10;
+let b=290;
+var c=(a*b);
+console.log("the product of the "*a*" and "*b*" is:",c);
